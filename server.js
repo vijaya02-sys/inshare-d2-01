545275707ef0f6cb3659aa17c5d215023d46930e
vijaya <vilajaxmyai@gmail.com>
2022-09-01 19:32:57 +0530

    Add script.js

--- server.js
+++ server.js
@@ -1,36 +1,38 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 3000;
 const path = require('path');
 const cors = require('cors');
-
-const PORT = process.env.PORT || 3000;
-app.use(express.static('public'));
-app.use(express.json());
-
-const connectDB = require('./config/db');
-
-connectDB();
-
-
-// Cors
+// Cors 
 const corsOptions = {
-    origin: process.env.ALLOWED_CLIENTS.split(',')
-// ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
-
+  origin: process.env.ALLOWED_CLIENTS.split(',')
+  // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
 }
 
+// Default configuration looks like
+// {
+//     "origin": "*",
+//     "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+//     "preflightContinue": false,
+//     "optionsSuccessStatus": 204
+//   }
 
-// Template engine
+app.use(cors(corsOptions))
+app.use(express.static('public'));
+
+const connectDB = require('./config/db');
+connectDB();
+
+app.use(express.json());
+
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 
-
-
-// Routes
+// Routes 
 app.use('/api/files', require('./routes/files'));
 app.use('/files', require('./routes/show'));
 app.use('/files/download', require('./routes/download'));
 
-app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
-})
+
+app.listen(PORT, console.log(`Listening on port ${PORT}.`));
